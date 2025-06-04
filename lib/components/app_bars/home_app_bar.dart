df56d3baffd0b8c5a1828ee8435c9525c7eb2eab
Fazilios <ptfazil02@gmail.com>
2025-06-04 22:51:10 +0530

    fazil

--- lib/components/app_bars/home_app_bar.dart
+++ lib/components/app_bars/home_app_bar.dart
@@ -1,1 +1,43 @@
 
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../constants.dart';
+
+class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final VoidCallback? onProfileTap;
+
+  const HomeAppBar({Key? key, this.onProfileTap}) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leadingWidth: 200,
+      leading: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(
+            appLogoSmall,
+            scale: 1.3,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: InkWell(
+            onTap: onProfileTap,
+            child: CircleAvatar(
+              backgroundColor: const Color(0xffF2F2F2),
+              child: SvgPicture.asset('$svgPath/userProfileICon.svg'),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/src/agent/dashboard/agent_home_screen.dart
+++ lib/src/agent/dashboard/agent_home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:cashlink/components/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../components/app_bars/home_app_bar.dart';
 
 class AgentHomeScreen extends StatelessWidget {
   static const routeName = '/agent_home';
@@ -8,22 +12,20 @@ class AgentHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
 
-        title: const Text('Agent Dashboard'),
+    return Scaffold(
+      
+      appBar: HomeAppBar(
+        onProfileTap: () {
+
+          },
       ),
       body: Center(
         child: Text(
           'Welcome to the Agent Dashboard',
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Action for the button
-        },
-        child: const Icon(Icons.add),
-      ),
+
 
     );
   }
